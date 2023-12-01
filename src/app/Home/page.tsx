1500e6d11f1222a2026d3c0ce68ce64b5ce485d8
David Marom <davidma@matrix.co.il>
2023-12-01 16:03:22 +0200

    done

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -4,9 +4,20 @@ import { Button, Popconfirm, DatePicker } from "antd";
 import { Table } from 'antd'
 import http from '../../services/http';
 
+const { RangePicker } = DatePicker;
+
+const timeStamptoUnixTime = (timestamp: number) => {
+    return new Date(timestamp).getTime();
+}
+
 const Home = () => {
     const [movies, setMovies] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [range, setRange] = useState([0, 0]);
+
+    const handleChange = (pagination: any, filters: any, sorter: any) => {
+        setRange(filters.date);
+    };
 
     useEffect(() => {
         const lsMovies = localStorage.getItem('movies');
@@ -20,55 +31,81 @@ const Home = () => {
     }, []);
 
 
-const epochToString = (epoch: number) => {
-    const date = new Date(epoch);
-    return date.toLocaleDateString();
-}
-
-const columns = [
-    {
-        title: '',
-        dataIndex: '_id',
-        key: '_id',
-        width: "1px",
-        render: () => '',
-    },
-    {
-        title: 'Title',
-        width: "300px",
-        dataIndex: 'title',
-        key: 'title',
-    },
-    {
-        title: 'Date',
-        dataIndex: 'date',
-        key: 'date',
-        sorter: (a: any, b: any) => new Date(a.date).getTime() - new Date(b.date).getTime(),
-        render: (date: number) => epochToString(date)
-    },
-    {
-        title: 'Description',
-        dataIndex: 'description',
-        key: 'description',
-    },
-    {
-        title: 'Duration',
-        dataIndex: 'duration',
-        key: 'duration',
-        render: (duration: number) => `${duration} min`
-    },
-    {
-        title: 'Action',
-        key: 'action',
-        render: (text: any, record: any) => (
-            <Button style={{ backgroundColor: '#2196F3', color: 'white' }} type="primary" onClick={() => {
-                console.log(record._id)
-            }}
-            >Purchase</Button>
-        ),
-        
+    const epochToString = (epoch: number) => {
+        const date = new Date(epoch);
+        return date.toLocaleDateString();
     }
-];
+
+    const columns = [
+        {
+            title: '',
+            dataIndex: '_id',
+            key: '_id',
+            width: "1px",
+            render: () => '',
+        },
+        {
+            title: 'Title',
+            width: "300px",
+            dataIndex: 'title',
+            key: 'title',
+        },
+        {
+            title: 'Date',
+            dataIndex: 'date',
+            key: 'date',
+            sorter: (a: any, b: any) => new Date(a.date).getTime() - new Date(b.date).getTime(),
+            render: (date: number) => epochToString(date),
+
+            filterDropdown: (props: any) => {
+                const { setSelectedKeys, selectedKeys, confirm, clearFilters } = props;
+                return (
+                    <div style={{ padding: 8 }}>
+                        <RangePicker
+                            value={selectedKeys}
+                            onChange={(dates: any) => {
+                                setSelectedKeys(dates);
+                                confirm();
+                            }}
+                            onOk={confirm}
+                        // onClear={clearFilters}
+                        />
+                    </div>
+                );
+            },
+            filterIcon: (filtered: any) => (
+                <span style={{ color: filtered ? '#1890ff' : undefined }}>
+                    Filter
+                </span>
+            ),
+            onFilter: (value: any, record: any) => {
+                return record.date >= timeStamptoUnixTime(range[0]) && record.date <= timeStamptoUnixTime(range[1])
+            }
+
+        },
+        {
+            title: 'Description',
+            dataIndex: 'description',
+            key: 'description',
+        },
+        {
+            title: 'Duration',
+            dataIndex: 'duration',
+            key: 'duration',
+            render: (duration: number) => `${duration} min`
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            render: (text: any, record: any) => (
+                <Button style={{ backgroundColor: '#2196F3', color: 'white' }} type="primary" onClick={() => {
+                    console.log(record._id)
+                }}
+                >Purchase</Button>
+            ),
+
+        }
+    ];
 
     return (
         <>
@@ -76,6 +113,9 @@ const columns = [
                 columns={columns}
                 dataSource={movies}
                 rowKey="_id"
+
+                onChange={handleChange}
+
 
                 pagination={{
                     showSizeChanger: true,
