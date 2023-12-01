0ca7abb795a30048d1846e03a88eb49d33d8f658
David Marom <davidma@matrix.co.il>
2023-12-01 21:35:49 +0200

    places marked

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,16 +1,51 @@
 "use client"
 import { useRouter } from 'next/navigation';
-
+import { epochToString } from "@/utils";
+import { useEffect, useState } from 'react';
 
 const MoviePage = ({ params }: { params: { id: string } }) => {
 
-    const movies= localStorage.getItem('movies');
+    const movies = localStorage.getItem('movies');
     const movie = movies ? JSON.parse(movies).find((movie: any) => movie._id === params.id) : null;
+
+    const seats = movie ? movie.seats : null;
+    const dataArray = Array.from({ length: 100 });
+
+    const [populatedArray, setPopulatedArray] = useState(dataArray);
+
+
+    useEffect(() => {
+        const aaa = dataArray.map((el, index) => {
+            if (seats[index + 1] === '1') {
+                return '1'
+            }
+            return '0'
+        })
+        setPopulatedArray(aaa);
+    }, []);
+
+
 
     return (
         <div>
-            <h1>{params.id}</h1>
-            
+            <h2>{movie.title}</h2>
+            <div className="movie-details">
+                <p><b>Description:</b> {movie.description}</p>
+            </div>
+            <div className="movie-details">
+                <p><b>Duration: </b>{movie.duration} min.</p>
+                <div className="w-space" />
+                <p><b>Date: </b>{epochToString(movie.date)}</p>
+            </div>
+
+            <div className='grid-10-10'>
+
+                {
+                    populatedArray.map((el, index) => <div key={index}>{el === '1' ? '' : 'Free'}</div>)
+                }
+
+            </div>
+
         </div>
     );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
 "use client"
+
+import Link from "next/link"
+
 export default function Home() {
 
-
-
   return (
-      <h1>Welcome</h1>
+    <>
+      <div className="v-space" />
+
+      <div className="row">
+        <h1>Welcome</h1>
+      </div>
+
+      <div className="row">
+        <Link href="/Home">Go to the movie list</Link>
+      </div>
+
+
+    </>
   )
 }
