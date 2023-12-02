31b3e276e2cde0bc58d95ad6d5de5bd22637f4da
David Marom <davidma@matrix.co.il>
2023-12-02 10:31:19 +0200

    PUT is ready

--- src/app/api/movies/route.ts
+++ src/app/api/movies/route.ts
@@ -30,14 +30,11 @@ export async function POST(request: Request) {
 
 export async function PUT(request: Request) {
     const body = await request.json();
-    console.log('============');
-    console.log(body);
+    const newBody = { seats: body.seats }
     const client = await connectDatabase();
-    const documents = await updateDocument(client, 'movies', { _id: body._id }, body);
+    const documents = await updateDocument(client, 'movies', body._id, { $set: newBody });
     client.close();
     return new Response(JSON.stringify(documents), {
         headers: { 'Content-Type': 'application/json' },
     });
-    
-
 }
