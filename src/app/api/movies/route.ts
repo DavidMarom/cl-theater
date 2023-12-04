03057973b155caa851aa57a223f783946a211249
David Marom <davidmarom.pro@gmail.com>
2023-12-04 08:47:30 +0200

    done

--- src/app/api/movies/route.ts
+++ src/app/api/movies/route.ts
@@ -36,16 +36,6 @@ export async function PUT(request: Request) {
     });
 }
 
-// export async function POST(request: Request) {
-//     const body = await request.json();
-//     const client = await connectDatabase();
-//     const documents = await updateDocument(client, 'movies', body._id, { $set: body });
-//     client.close();
-//     return new Response(JSON.stringify(documents), {
-//         headers: { 'Content-Type': 'application/json' },
-//     });
-// }
-
 export async function DELETE(request: Request) {
     const body = await request.json();
     const client = await connectDatabase();
